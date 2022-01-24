4f846732dd6468775571add4ea864ef0bcc1be32
Roger Wittmer <rkwittmer@knox.edu>
2022-01-24 13:16:15 -0600

    movement, basic upgrade stuff, life system, heightwarning, backgrounds, foregrounds etc

---
+++ Assets/GameStuffFromKoji/Assets/SortingOrderScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SortingOrderScript : MonoBehaviour
+{
+
+    public GameObject my_fish;
+    public GameObject fin_item;
+
+    ctrl TheScript;
+    GameObject TheGameController;
+
+
+    void Start()
+    {
+        fin_item.GetComponent<Image>().enabled = false;
+
+        TheGameController = GameObject.Find("blobfish");
+        TheScript = TheGameController.GetComponent<ctrl>();
+
+
+    }
+
+    void Update()
+    {
+
+       if (my_fish.transform.position.x > 3)
+        {
+            fin_item.GetComponent<Image>().enabled = true;
+            TheScript.speed = 40;
+        }
+        else
+        {
+            fin_item.GetComponent<Image>().enabled = false;
+            TheScript.speed = 20;
+        }
+    }
+
+}
---
+++ Assets/GameStuffFromKoji/Assets/ctrl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class ctrl : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+
+}
