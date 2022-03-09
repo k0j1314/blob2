13dcc85a1d1f21cad17fb59967e5893703d66ca4
Roger Wittmer <rkwittmer@knox.edu>
2022-03-08 19:59:19 -0600

    bugs: bengame health didnt reset on fail, koji's red screen was offset, bengame background was too small, winscreen centered text

--- Assets/Ben's Scenes/Shooting Mini Game/Hearts.cs
+++ Assets/Ben's Scenes/Shooting Mini Game/Hearts.cs
@@ -12,6 +12,7 @@ public class Hearts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        heart = 5;
         healthRemaining = heart;
         heart = healthRemaining;
     }
